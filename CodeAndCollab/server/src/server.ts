531a88d6bd3acebeb83541f4ec36b245bf58f17f
Nagesh2003 <codeinovator@gmail.com>
2025-09-11 13:11:07 +0530

    Add

--- CodeAndCollab/server/src/server.ts
+++ CodeAndCollab/server/src/server.ts
@@ -292,6 +292,13 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// server.listen(PORT,'0.0.0.0' () => {
+//   console.log(`Listening on port ${PORT}`);
+// });
+
+server.listen(
+  { port: Number(PORT), host: "0.0.0.0" },
+  () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+  }
+);

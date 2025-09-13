9260c05486138b6d7722a9f2b960c2ae06902627
Nagesh2003 <codeinovator@gmail.com>
2025-09-13 10:46:31 +0530

    Add

--- CodeAndCollab/client/src/api/pollinationsApi.ts
+++ CodeAndCollab/client/src/api/pollinationsApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
-const pollinationsBaseUrl = "https://text.pollinations.ai"
+const pollinationsBaseUrl = "https://text.pollinations.ai/Hello%20there%20how%20are%20you%3F"
 
 const instance: AxiosInstance = axios.create({
     baseURL: pollinationsBaseUrl,
